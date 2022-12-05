1a3be1275770c7c40428dd2abfa72b6667cfb345
Gabriel Andrade <gaa_andraade@hotmail.com>
2022-12-04 21:38:50 -0300

    Descricao

--- Projeto Faculdade.c
+++ Projeto Faculdade.c
@@ -10,5 +10,16 @@ int main(){
 
    altura = pow(altura,2);
    IMC = peso/altura;
+
    printf("Valor IMC: %.2f", IMC);
+
+   printf("\n\nIMC = peso / (altura * altura)");
+     printf("\n\nIMC calculado   Situacao");
+     printf("\n------------------------------");
+     printf("\nMenos de 20     Abaixo do peso");
+     printf("\n20 >= IMC < 25  Peso Normal");
+     printf("\n25 >= IMC < 30  Acima do peso");
+     printf("\n30 >= IMC < 34  Obeso");
+     printf("\nAcima de 34     Muito Obeso");
+     printf("\n------------------------------");
 }
