3c03e0be95c9d4fa049a5c184ec49bf911c6d234
Gabriel Andrade <gaa_andraade@hotmail.com>
2022-12-04 21:33:48 -0300

    Projeto Inicial
    Fabio basso

---
+++ Projeto Faculdade.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+   float IMC,peso,altura;
+   printf("Digite o peso\n> ");
+   scanf("%f",&peso);
+   printf("Digite a altura\n> ");
+   scanf("%f",&altura);
+
+   altura = pow(altura,2);
+   IMC = peso/altura;
+}
